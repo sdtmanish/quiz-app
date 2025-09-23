6d8b61e613282945b17ec0228809b517e51e9d46
Manish <sdt.manishbailwal@gmail.com>
2025-09-23 12:13:41 +0530

    shifted from polling to websocket

--- app/lib/socket.js
+++ app/lib/socket.js
@@ -5,4 +5,6 @@ const URL = process.env.NEXT_PUBLIC_BACKEND_URL || "https://quizhubapi.onrender.
 
 export const socket = io(URL, {
   autoConnect: false,
+  transports:["websocket"],      //only allow websocket
+  upgrade: false,     //prevent fallback to polling
 });
